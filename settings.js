f10e08b71632b9709c0818034829ab9c0b3ca377
vishdollie <110341536+vishdollie@users.noreply.github.com>
2022-10-11 08:29:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,26 +54,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94714188730'] //ur owner number
-global.ownername = "nipuna rangana" //ur owner name
-global.ytname = "YT: 𝐂𝐈𝐁𝐄𝐑 𝐓𝐄𝐂𝐇" //ur yt chanel name
+global.vcardowner = ['0711920394'] //ur owner number
+global.ownername = "𝚅𝙸𝚂𝙷𝙳𝙾𝙻𝙻𝙸𝙴" //ur owner name
+global.ytname = "YT: 𝐕𝐢𝐬𝐡𝐝𝐨𝐥𝐥𝐢𝐞-𝐖𝐇-𝐁𝐨𝐭" //ur yt chanel name
 global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy
-global.owner = ['94714188730'] //ur number 
-global.ownertag = '94714188730' //ur tag number
-global.botname = '📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶' //ur bot name
+global.owner = ['0711920394'] //ur number 
+global.ownertag = '0711920394' //ur tag number
+global.botname = '📍🎯𝗩𝗜𝗦𝗛𝗗𝗢𝗟𝗟𝗜𝗘 𝗪𝗛-𝗕𝗢𝗧🎯📍' //ur bot name
 global.linkz = "https://youtu.be/3J9-8U8St3U" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/3J9-8U8St3U" //ur website to be displayed
 global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark packname
-global.author = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark author
+global.packname = "📶 𝗩𝗜𝗦𝗛𝗗𝗢𝗟𝗟𝗜𝗘 𝗪𝗛-𝗕𝗢𝗧 📶 " //ur sticker watermark packname
+global.author = "📶 𝚅𝙸𝚂𝙷𝙳𝙾𝙻𝙻𝙸𝙴 📶" //ur sticker watermark author
 global.menumoji ="💖"
 global.menuimg = "https://i.ibb.co/0mDLtyr/f2337149532299-58b7611d91faf.jpg" //ur alive img link [https://i.ibb.co]
-global.alive = "https://raw.githubusercontent.com/Nipuna-apps/Blue-Lion-Bot-V2/main/LioneMedia/video/Lione-bot.mp4" //ur alive video or image link [github.com]
+global.alive = "https://raw.githubusercontent.com/Vish/Vishdollie WH-Bot-V2/main/LioneMedia/video/Lione-bot.mp4" //ur alive video or image link [github.com]
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
@@ -81,7 +81,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['94714188730'] //ur premium numbers
+global.premium = ['0711920394'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
